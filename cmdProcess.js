fe668ce06043ba1b179b7c3176733b3f059492ad
abhiskull <abhishek.k.6295@gmail.com>
2020-05-23 22:16:41 +0530

    final commit with all requirment

--- cmdProcess.js
+++ cmdProcess.js
@@ -16,12 +16,14 @@ var allotParking = function(vechicleDetail){
     var searchSlot = _.findIndex(parkingSlot, function(eachSlot){
         return eachSlot == null
     });
+    if(searchSlot === -1){
+        console.log("Sorry, parking lot is full");
+    }
     parkingSlot[searchSlot] = vechicleDetail;
-    return;
+    return searchSlot;
 };
 
 var createParkingSlot = function(reqSlots) {
-    
     for(var i= 1; i<parseInt(reqSlots)+1; i++){
         parkingSlot.push(null);
     }
@@ -29,19 +31,53 @@ var createParkingSlot = function(reqSlots) {
 }
 
 var leaveParkingSlot = function(leavingSlotNumber){
-    if(parkingSlot[parseInt(leavingSlotNumber)] == undefined){
+    if(parkingSlot[parseInt(leavingSlotNumber)-1] == undefined){
         console.log("Requested slot are not available");
     }else{
-        parkingSlot[parseInt(leavingSlotNumber)] = null; 
+        parkingSlot[parseInt(leavingSlotNumber)-1] = null;
     }
 }
 
 var getStatus = function(){
     console.log("Slot" + "   " + "Regestration" + "   " + "Color");
     console.log("--------------------------------");
+    var i = 1;
     _.forEach(parkingSlot, function(eachSlot){
-        console.log(eachSlot);
+        if(eachSlot != null){
+            console.log("   " + i++ + "    " + eachSlot.regNumber + "    " + eachSlot.color);
+        }else{
+            console.log("   " + i++ + "        " + null + "         " + null);
+        }
     })
+}
+
+var getRegNumberByColor = function(reqColor){
+    var listOfReg = [];
+    _.forEach(parkingSlot, function(eachSlot){
+        if(eachSlot.color == reqColor){
+            listOfReg.push(eachSlot.regNumber);
+        }
+    })
+    return listOfReg;
+}
+
+var getListOfSlotByColor = function(reqColor){
+    var listOfSlots = [];
+    for(var i = 0; i<parkingSlot.length; i++){
+        if(parkingSlot[i].color == reqColor){
+            listOfSlots.push(i+1);
+        }
+    }
+    return listOfSlots;
+}
+
+var getSlotNoForReg = function(reqRegNo){
+    var getRegNumber = _.findIndex(parkingSlot, function(eachParkingSlot){
+        if(eachParkingSlot != null){
+            return eachParkingSlot.regNumber == reqRegNo;
+        }
+    })
+    return getRegNumber;
 }
 
 cmdProcess.processParking = function(cmd, callback){
@@ -56,8 +92,9 @@ cmdProcess.processParking = function(cmd, callback){
             var vechicleDetail = {};
             vechicleDetail.regNumber = splitInputCMD[1];
             vechicleDetail.color = splitInputCMD[2];
-            allotParking(vechicleDetail);
-            console.log(parkingSlot);
+            var slotNumber = allotParking(vechicleDetail);
+            slotNumber++;
+            console.log("Alloted slot number", slotNumber);
             parking();
             break;
         case 'leave':
@@ -67,10 +104,39 @@ cmdProcess.processParking = function(cmd, callback){
             break;
         case 'status':
             getStatus();
+            parking();
+            break;
+        case 'registration_numbers_for_cars_with_colour':
+            var listofRegNo = getRegNumberByColor(splitInputCMD[1]);
+            if(listofRegNo.length){
+                var strListofReg = listofRegNo.toString();
+                console.log(strListofReg); 
+            }else{
+                console.log("NO Data found");
+            }
+            parking();
             break;  
+        case 'slot_numbers_for_cars_with_colour':
+            var listOfSlot = getListOfSlotByColor(splitInputCMD[1]);
+            if(listOfSlot.length){
+                var strListOfSlot = listOfSlot.toString();
+                console.log(strListOfSlot);
+            }else{
+                console.log("NO Data found");
+            }
+            parking();
+            break;
+        case 'slot_number_for_registration_number':
+            var slotNumber = getSlotNoForReg(splitInputCMD[1]);
+            if(slotNumber == -1){
+                console.log("Not Found");
+            }else{
+                console.log(parseInt(slotNumber) + 1);
+            }
+            parking();
         default:
-            
-
+            console.log("wrong input provided by user");
+            process.exit(0);
     }
 }
 
